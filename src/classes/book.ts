5d38691d33000352453c74069b1b880cdc2f4570
Paolo Iommarini <paolo.iommarini@live.com>
2021-05-22 11:34:25 +0200

    Added LinkAction

--- src/classes/book.ts
+++ src/classes/book.ts
@@ -4,10 +4,14 @@ export class Book {
     public serie: string = '';
     public index: number = 0;
     public title: string = '';
-    public author: string = '';
+    public authors: string[] = [];
+    public illustrators: string[] = [];
+    public publisher: string = '';
+    public version: string = '';
 
     public pages: Page[] = [];
-    
+
     constructor() {
+        
     }
 }
--- src/classes/page.ts
+++ src/classes/page.ts
@@ -1,15 +1,26 @@
-export class Action {
-    constructor() {
+export abstract class Action {
+    public text: String;
 
+    constructor(text: string) {
+        this.text = text;
     }
 } // Action
 
+export class LinkAction extends Action {
+    public PageId: string = '';
+
+    constructor(text: string, pageId: string) {
+        super(text);
+
+        this.PageId = pageId;
+    }
+} // LinkAction
 
 export class Page {
-    public number: number = 0;
+    public id: string = '';
     public text: string = '';
     public actions: Action[] = [];
-    
+
     constructor() {
 
     }
